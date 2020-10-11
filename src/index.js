33b7a8a54ec17df23e3703dc271ebbf0b1a54c53
Ritik Jain <ritikjain1002@gmail.com>
2020-10-11 12:03:17 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import createBrowserHistory from 'history/createBrowserHistory';
 import Header from './components/Header';
 import App from './components/App';
 import SocialProfiles from './components/SocialProfiles';
+import MusicMaster from './projects/music-master/src';
 import './index.css';
 
 ReactDOM.render(
     <Router history = {createBrowserHistory()}>
         <Switch>
             <Route exact path = "/" render = {() => <Header><App /></Header>}/>
-            <Route exact path = "/contact" render = {() => <Header><SocialProfiles /></Header>}/>
+            <Route path = "/contact" render = {() => <Header><SocialProfiles /></Header>}/>
+            <Route path = "/music-master" render = {() => <Header><MusicMaster /></Header>}/>
         </Switch>
     </Router>,
     document.getElementById('root')
